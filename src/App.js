b64d0896ced1073f618aa66c61a10c4a101fd27b
Kekelberi <alexei.gaponov@gmail.com>
2022-03-19 14:04:14 +0300

    save

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import Model from './model/Test'
 
 function App() {
   return (
-    <div >
-      start
-    </div>
-  );
+    <Canvas>
+      <OrbitControls />
+      <ambientLight intensity={0.6} />
+      <directionalLight intensity={0.6} />
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+    </Canvas>
+  )
 }
 
-export default App;
+export default App
